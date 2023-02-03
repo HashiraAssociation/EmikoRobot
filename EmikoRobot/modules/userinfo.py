a92aad4251b33ac040d559bdaa5d58a7b804d7db
HashiraAssociation <118347352+HashiraAssociation@users.noreply.github.com>
2023-02-03 18:15:46 +0530

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -445,9 +445,9 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>╔═━「 Current Victor Nikiforov Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    stats = "<b>╔═━「 Current Mitsuri  Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    result += "\n<b>╘═━「 Powered By @Hashira_Association 」</b>"
+    result += "\n<b>╘═━「 Powered By @Hashira_techdivision 」</b>"
     update.effective_message.reply_text(
         result,
         parse_mode=ParseMode.HTML, 
